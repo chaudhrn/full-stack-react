ccf767c786991d7c699b479353ccebf016832358
Nauman Chaudhry <chaudhrn@oregonstate.edu>
2021-06-21 23:13:24 +0100

    L1

---
+++ lesson1/back-end-template/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+})
+
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080');
+})
